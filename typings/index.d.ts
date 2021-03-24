d4aee456fa59d0de06a7b7cf5d3ba2b3b19f8f8a
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-03-24 18:32:01 +0000

    site

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Client } from './structures/client/Client.js';
-export { Command } from './structures/commands/Command.js';
-export { SlashCommand } from './structures/slash/Slash.js';
-export { ApplicationCommandOptionType } from './structures/slash/SlashTypes.js';
