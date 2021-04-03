f5ab9e03e42514fac2afca8d2add5822a337995f
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-04-03 01:05:01 +0100

    api tweek

--- src/structures/rest/REST.ts
+++ src/structures/rest/REST.ts
@@ -1,6 +1,6 @@
 import APIRequest from './APIRequest.js';
 
-export default function endpointConstructor (auth: string) {
+export default function (auth: string) {
     const endpoint = [ 'https://discord.com/api/v8' ];
 
     const handler = {
@@ -28,42 +28,3 @@ export default function endpointConstructor (auth: string) {
 
     return new Proxy(() => {}, handler);
 }
-
-// export class API {
-//     constructor(token: string | null) {
-//         this.auth = 'Bot ' + token;
-//     }
-
-//     private auth: string;
-
-//     protected get discord() {
-//         return function (auth: string) {
-//             const endpoint = [ 'https://discord.com/api/v8' ];
-
-//             const handler = {
-//                 get(target, name) {
-//                     if (name === 'toString') return () => endpoint.join('/');
-
-//                     if (['get', 'post', 'patch', 'delete'].includes(name)) return async (options: any = {}) => {
-//                         if (!options.headers) options.headers = {};
-
-//                         if (auth) options.headers['Authorization'] = auth;
-
-//                         return new APIRequest(name, endpoint.join('/'), options).make();
-//                     };
-
-//                     endpoint.push(name);
-
-//                     return new Proxy(() => {}, handler);
-//                 },
-//                 apply(target, that, args) {
-//                     endpoint.push(...args);
-
-//                     return new Proxy(() => {}, handler);
-//                 }
-//             };
-
-//             return new Proxy(() => {}, handler);
-//         }(this.auth);
-//     }
-// }
