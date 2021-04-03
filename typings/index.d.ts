44de86fc65ebb1e6621254e7fa924205cd048021
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-04-03 23:26:08 +0100

    hmm

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,3 @@
 export { Client, ClientOptions } from './structures/client/Client.js';
 export { Command, CommandInfo } from './structures/commands/Command.js';
 export { SlashCommand } from './structures/slash/Slash.js';
-export { MessageEmbed } from 'discord.js';
