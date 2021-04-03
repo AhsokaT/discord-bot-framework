66e80388bef37168b815f6fef67993b3d3c24427
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-04-03 23:51:28 +0100

    exporting all

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SlashCommand = exports.Command = exports.Client = void 0;
+exports.DiscordJS = exports.SlashCommand = exports.Command = exports.Client = void 0;
 var Client_js_1 = require("./structures/client/Client.js");
 Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return Client_js_1.Client; } });
 var Command_js_1 = require("./structures/commands/Command.js");
 Object.defineProperty(exports, "Command", { enumerable: true, get: function () { return Command_js_1.Command; } });
 var Slash_js_1 = require("./structures/slash/Slash.js");
 Object.defineProperty(exports, "SlashCommand", { enumerable: true, get: function () { return Slash_js_1.SlashCommand; } });
+exports.DiscordJS = require("discord.js");
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Client, ClientOptions } from './structures/client/Client.js';
 export { Command, CommandInfo } from './structures/commands/Command.js';
 export { SlashCommand } from './structures/slash/Slash.js';
+export * as DiscordJS from 'discord.js';
