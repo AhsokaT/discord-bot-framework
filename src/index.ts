8cda1ee32f53a898fa589e3217de3c448057453c
Patrick Lynch <patrick.2004.lynch@gmail.com>
2021-06-10 17:54:30 +0100

    exports version and updated DJS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import Client, { ClientOptions } from './client/Client.js';
 import GuildCommand, { GuildCommandCallback, GuildCommandProperties } from './structs/commands/GuildCommand.js';
 import DMCommand, { DMCommandCallback, DMCommandProperties } from './structs/commands/DMCommand';
+import UniversalCommand, { CommandCallback as UniversalCommandCallback, CommandProperties as UniversalCommandProperties } from './structs/commands/Command.js';
 import { CommandManagerOptions, CommandResolvable } from './structs/CommandManager.js';
 import { ApplicationCommandConstructorOptions, ApplicationCommandCallback, ApplicationCommandConstructor, ApplicationCommandResolvable } from './structs/ApplicationCommandManager.js';
 
+const Version = '2.0.0';
+
 export {
+    Version,
     Client,
     ClientOptions,
     GuildCommand,
@@ -14,6 +18,9 @@ export {
     DMCommand,
     DMCommandCallback,
     DMCommandProperties,
+    UniversalCommand,
+    UniversalCommandCallback,
+    UniversalCommandProperties,
     CommandManagerOptions,
     ApplicationCommandConstructorOptions,
     ApplicationCommandCallback,
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,8 @@
 import Client, { ClientOptions } from './client/Client.js';
 import GuildCommand, { GuildCommandCallback, GuildCommandProperties } from './structs/commands/GuildCommand.js';
 import DMCommand, { DMCommandCallback, DMCommandProperties } from './structs/commands/DMCommand';
+import UniversalCommand, { CommandCallback as UniversalCommandCallback, CommandProperties as UniversalCommandProperties } from './structs/commands/Command.js';
 import { CommandManagerOptions, CommandResolvable } from './structs/CommandManager.js';
 import { ApplicationCommandConstructorOptions, ApplicationCommandCallback, ApplicationCommandConstructor, ApplicationCommandResolvable } from './structs/ApplicationCommandManager.js';
-export { Client, ClientOptions, GuildCommand, GuildCommandCallback, GuildCommandProperties, CommandResolvable, DMCommand, DMCommandCallback, DMCommandProperties, CommandManagerOptions, ApplicationCommandConstructorOptions, ApplicationCommandCallback, ApplicationCommandConstructor, ApplicationCommandResolvable };
+declare const Version = "2.0.0";
+export { Version, Client, ClientOptions, GuildCommand, GuildCommandCallback, GuildCommandProperties, CommandResolvable, DMCommand, DMCommandCallback, DMCommandProperties, UniversalCommand, UniversalCommandCallback, UniversalCommandProperties, CommandManagerOptions, ApplicationCommandConstructorOptions, ApplicationCommandCallback, ApplicationCommandConstructor, ApplicationCommandResolvable };
